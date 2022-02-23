2e455997a4808267fce83c3eb89ccf999446f812
Layssa Aragão <oilayssaa@gmail.com>
2022-02-23 18:46:09 -0300

    filtro ordem alfa

--- src/data.js
+++ src/data.js
@@ -1,15 +1,4 @@
-/* estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};
-
-*/
-
+//////////FILTRO GÊNERO
 export const filtroGenero = (dados, condition) => {
   const resultadoGenero = dados.filter(
     (character) => character.gender.toLowerCase() === condition.toLowerCase()
@@ -24,22 +13,31 @@ toLowerCase -> transformar todos os valores para letras minusculas, pq no array 
 
 - Como sabe quem é male e female -> e.target.value no main.js) 
 */
-
+///////////PORCENTAGEM
 export const calcularPorcentagem = (total, portion) => {
   const porcentagem = Math.round((portion * 100) / total);
   return porcentagem;
 };
-
+///////////FILTRO STATUS DE VIDA
 export const statusFilter = (dados, condition) => {
   const statusResult = dados.filter(
     (character) => character.status.toLowerCase() === condition.toLowerCase()
   );
   return statusResult;
 };
-
-export const statusSpecies = (dados, condition) => {
+///////////FILTRO ESPÉCIES
+export const speciesFilter = (dados, condition) => {
   const speciesResult = dados.filter(
     (character) => character.species.toLowerCase() === condition.toLowerCase()
   );
   return speciesResult;
+};
+
+///////////ORDEM ALFABÉTICA: A - Z & Z - A
+export const orderAlfaFilter = (dados, order) => {
+  if (order === "a-z") {
+    return dados.sort((a, z) => (a.name > z.name ? 1 : -1));
+  } else {
+    return dados.sort((a, z) => (a.name > z.name ? -1 : 1));
+  }
 };
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,57 +1,52 @@
-import { filtroGenero, calcularPorcentagem} from '../src/data.js';
+import { filtroGenero, calcularPorcentagem } from "../src/data.js";
 
 const Personagens = [
   {
-     "name": "Bearded Lady",
-     "status": "Dead",
-     "species": "Alien",
-     "gender": "Female",
-   },
-   {
-     "name": "Boobloosian",
-     "status": "Dead",
-     "species": "Alien",
-     "gender": "unknown",
-   },
-   {
-     "name": "Rick Sanchez",
-     "status": "Alive",
-     "species": "Human",
-     "gender": "Male",
-   },
-   {
-     "name": "Morty Smith",
-     "status": "Alive",
-     "species": "Human",
-     "gender": "Male",
-   }
+    name: "Bearded Lady",
+    status: "Dead",
+    species: "Alien",
+    gender: "Female",
+  },
+  {
+    name: "Boobloosian",
+    status: "Dead",
+    species: "Alien",
+    gender: "unknown",
+  },
+  {
+    name: "Rick Sanchez",
+    status: "Alive",
+    species: "Human",
+    gender: "Male",
+  },
+  {
+    name: "Morty Smith",
+    status: "Alive",
+    species: "Human",
+    gender: "Male",
+  },
 ];
 
 // Teste do Filtro Genero Male
 
-describe('filtroGenero', () => {
-  it('Deverá ser uma função', () => {
-    expect(typeof filtroGenero).toBe('function');
+describe("filtroGenero", () => {
+  it("Deverá ser uma função", () => {
+    expect(typeof filtroGenero).toBe("function");
   });
 
-  it('Deverá filtrar pelo gênero Male', () => {
-    const expected = filtroGenero(Personagens.length)
-    expect (expected.gender).tobe('Male'); 
-  });
-});
-
-
-//Teste da Porcentagem
-
-describe('calcularPorcentagem', () => {
-  it('Deverá ser uma função', () => {
-    expect(typeof calcularPorcentagem).toBe('function');
-  });
-
-  it('Deverá retornar 50% de personagens masculinos', () => {
-    const expected = calcularPorcentagem(Personagens.length, 2)
-    expect (expected).toEqual(50) //A função .toEqual(valor) testa recursivamente cada valor do objeto ou array.
+  it("Deverá filtrar pelo gênero Male", () => {
+    const expected = filtroGenero(Personagens.length);
+    expect(expected.gender).tobe("Male");
   });
 });
 
+describe("calcularPorcentagem", () => {
+  it("Deverá ser uma função", () => {
+    expect(typeof calcularPorcentagem).toBe("function");
+  });
 
+  it("Deverá retornar 50% de personagens masculinos", () => {
+    const expected = calcularPorcentagem(Personagens.length, 2);
+    expect(expected).toEqual(50); //A função .toEqual(valor) testa recursivamente cada valor do objeto ou array.
+  });
+});
