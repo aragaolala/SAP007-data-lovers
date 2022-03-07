9888541fbbc733acc8aff6bd9974b2db14e6c6f5
Layssa Aragão <oilayssaa@gmail.com>
2022-03-07 19:33:59 -0300

    readme em construcao

--- src/data.js
+++ src/data.js
@@ -8,19 +8,26 @@ export const filtroGenero = (dados, condition) => {
 };
 
 /* 
-toLowerCase -> transformar todos os valores para letras minusculas, pq no array está
+- esse condition é a o que o user seleciona no selector - 
+passar o que ele selecionar para lower case e ser exatamente igual a filtragem (que tb
+  será passada para toLowerCase)
+(o js pega essa condition por meio do e.target.value no main.js / o dados = dados.result)
+- toLowerCase -> transformar todos os valores para letras minusculas, pq no array está
 "Male"/ Female e unknown - tá misturado
-
 - Como sabe quem é male e female -> e.target.value no main.js) 
 */
+
 ///////////PORCENTAGEM
 export const calcularPorcentagem = (total, fractionItem) => {
   const porcentagem = Math.round((fractionItem * 100) / total);
   return porcentagem;
 };
-// A função Math.round() retorna o valor de um número arredondado para o inteiro.
-//portion - a porção daquele item no total da array. 
-//Ex: 30 personagens femininas dentro de 100 personagens totais no array.
+/* como sabe quem é total (argumento 1) e quem é fractionItem (argumento 2) ? Lá no data.js 
+- total = dados.results.length - fractionItem =  resultadoGenero.length
+- A função Math.round() retorna o valor de um número arredondado para o inteiro.
+- portion - a porção daquele item no total da array. 
+- Ex: 30 personagens femininas dentro de 100 personagens totais no array.
+*/
 
 ///////////FILTRO STATUS DE VIDA
 export const statusFilter = (dados, condition) => {
@@ -43,8 +50,14 @@ export const orderAlfaFilter = (dados, order) => {
     return dados.sort((a, z) => (a.name > z.name ? 1 : -1));
   } else {
     return dados.sort((a, z) => (a.name > z.name ? -1 : 1));
-  }
+  } // linha 52 nao cobre no teste - rever
 };
+/*   
+- a "order" é pega pelo e.target.value no main.js - 
+- o e.target.value pega o valor selecionado pelo user
+-> -1 inverte a ordem / 1 - ordem ascendente 
+esse "?" = valor booleano - true
+*/
 
 //////////////////FUNÇÃO DE BUSCA POR NOME- FILTER()
 export const  searchName = (dados, condition) => {
@@ -52,3 +65,29 @@ export const  searchName = (dados, condition) => {
   return resultado;
 };
 
+/* esse
+if (order === "a-z") {
+    return dados.sort((a, z) => (a.name > z.name ? 1 : -1));
+  } else {
+    return dados.sort((a, z) => (a.name > z.name ? -1 : 1));
+  } // linha 52 nao cobre no teste - rever
+*
+
+ if (order === "a-z") {
+    return dados.sort((a, z) => { 
+      a.name > z.name
+      return -1;
+    } 
+
+  } if (order === "z-a") {
+    return dados.sort((a, z) => {
+    a.name < z.name
+    return 1;
+    }
+  }
+};
+
+
+
+
+*/
